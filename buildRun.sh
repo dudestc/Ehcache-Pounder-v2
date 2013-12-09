d609c7f60d838a2ab6b1b5ea2d9c519f8f2a4822
Stephan Grotz (stgr) <Stephan.Grotz@softwareag.com>
2013-12-09 15:36:16 +0100

    Updated to Version 0.4

--- buildRun.sh
+++ buildRun.sh
@@ -3,8 +3,10 @@
 mvn clean javadoc:javadoc assembly:assembly
 
 cd target
-tar -xvzf ehcache-pounder-v2-0.3-distribution.tar.gz 
-cd ehcache-pounder-v2-0.3
+tar -xvzf ehcache-pounder-v2-0.4-distribution.tar.gz 
+cd ehcache-pounder-v2-0.4
+
+cp ../../*.key .
 
 chmod a+x *.sh
 
